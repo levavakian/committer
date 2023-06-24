fcba3cb4ea713e7bb3ec744ba2dfe1f7a81b5818
Levon Avagyan <levavakian@gmail.com>
2023-06-24 13:50:25 +0200

    change name

--- app/.well-known/ai-plugin.json/route.ts
+++ app/.well-known/ai-plugin.json/route.ts
@@ -5,10 +5,10 @@ export const runtime = "edge";
 export async function GET(req: NextRequest) {
   const data = {
     "schema_version": "v1",
-    "name_for_model": "CommitterGPT",
-    "name_for_human": "CommitterGPT",
+    "name_for_model": "Committer",
+    "name_for_human": "Committer",
     "description_for_human": "Read, create, modify and delete files from Github repos.",
-    "description_for_model": "Use the CommitterGPT plugin to interact with Github repos. You can use it to read files, read the directory tree of the repo, create new files, overwrite existing files, append to existing files, and delete files. All operations are done with commits so they are non-destructive.",
+    "description_for_model": "Use the Committer plugin to interact with Github repos. You can use it to read files, read the directory tree of the repo, create new files, overwrite existing files, append to existing files, and delete files. All operations are done with commits so they are non-destructive.",
     "api": {
       "type": "openapi",
       "url": `${process.env.SELF_URL}.well-known/openapi.json`,
