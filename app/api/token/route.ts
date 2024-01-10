1237abb4fd867f7ceabe45533e5dc3f0003d68cf
Levon Avagyan <levavakian@gmail.com>
2024-01-10 18:52:07 -0500

    Update route.ts

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -4,9 +4,10 @@ import { getBearerToken } from  "@/app/lib/utils"
 export const runtime = "edge";
 
 export async function POST(req: NextRequest) {
-  const data = await req.json(content_type=None)
+  const url = new URL(req.url)
+  const code = url.searchParams.get("code")
 
-  const response = await getBearerToken(data.code)
+  const response = await getBearerToken(code)
   const token_data = await response.json()
 
   return NextResponse.json(token_data);
