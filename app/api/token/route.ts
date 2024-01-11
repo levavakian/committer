04f2c1b2ffa2b69d54dd47f1c8a63b06b952cd05
Levon Avagyan <levavakian@gmail.com>
2024-01-10 19:37:46 -0500

    Update route.ts

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -5,6 +5,7 @@ export const runtime = "edge";
 
 export async function POST(req: NextRequest) {
   const text = await req.text();
+  console.warn(text)
   const body = JSON.parse(text);
   const code = body.code;
   
