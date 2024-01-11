1ec5b4c72664f00b847c7208328aabd840e1467e
Levon Avagyan <levavakian@gmail.com>
2024-01-10 19:28:20 -0500

    Update route.ts

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -4,11 +4,18 @@ import { getBearerToken } from  "@/app/lib/utils"
 export const runtime = "edge";
 
 export async function POST(req: NextRequest) {
-  const url = new URL(req.url)
-  const code = url.searchParams.get("code")!
+  console.warn(req)
+  // Get the request body as text
+  const text = await req.text();
+
+  // Parse the text as JSON
+  const body = JSON.parse(text);
+
+  // Access the 'code' field from the parsed body
+  const code = body.code;
 
   console.warn(code)
-  const response = await getBearerToken(code)
+  const response = await getBearerToken(dcode)
   console.warn(response)
   const token_data = await response.json()
   console.warn(token_data)
